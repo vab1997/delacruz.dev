933822fb97a0b6db7a7c6c51fa885debeab00562
Dani de la Cruz <mail@danidev.es>
2019-06-17 12:52:16 +0200

    Adds analytics

---
+++ static/js/analytics.js
@@ -0,0 +1,12 @@
+const callToActions = window.document.querySelectorAll("[data-tracking]");
+
+if (callToActions && callToActions.length) {
+  callToActions.forEach(function(trackedElement) {
+    trackedElement.addEventListener("click", function() {
+      gtag("event", "click", {
+        event_category: "Call to Action",
+        event_label: trackedElement.dataset.tracking
+      });
+    });
+  });
+}
