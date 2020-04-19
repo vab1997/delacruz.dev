892615582319e4ccbcdd4d934cfe4d33a721be79
Dani de la Cruz <5173869+delacruz-dev@users.noreply.github.com>
2020-04-19 08:31:19 +0200

    resize badge image

--- components/mentoring-intro.js
+++ components/mentoring-intro.js
@@ -1,10 +1,10 @@
 function MentoringIntro() {
   return (
     <section className="hero is-medium is-primary is-bold">
-      <div class="hero-body mentoring">
-        <div class="container has-text-centered">
-          <h1 class="title is-size-1-desktop">Mentoring profesional</h1>
-          <h2 class="subtitle is-size-4-desktop">
+      <div className="hero-body mentoring">
+        <div className="container has-text-centered">
+          <h1 className="title is-size-1-desktop">Mentoring profesional</h1>
+          <h2 className="subtitle is-size-4-desktop">
             Para programadores y programadoras que quieran dar un salto en su carrera desarrollando software.
           </h2>
           <figure className="image is-128x128 avatar">
