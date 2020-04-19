b5222bc89042b278d09b672dc4e0f7bcebd22f83
Dani de la Cruz <5173869+delacruz-dev@users.noreply.github.com>
2020-04-19 08:41:19 +0200

    Improve text contrast

--- components/pricing.js
+++ components/pricing.js
@@ -10,16 +10,12 @@ function Pricing() {
                   <span className="icon is-large has-text-info is-spaced">
                     <i className="fas fa-3x fa-user-friends"></i>
                   </span>
-                  <h1 className="title is-size-4 is-spaced">
-                    Acompañamiento técnico
-                  </h1>
+                  <h1 className="title is-size-4 is-spaced">Acompañamiento técnico</h1>
                   <p className="is-spaced">
-                    Elige este plan si lo que necesitas es un compañero de vuelo
-                    que te ayude a mejorar técnicamente.
+                    Elige este plan si lo que necesitas es un compañero de vuelo que te ayude a mejorar técnicamente.
                   </p>
-                  <p className="list-title has-text-info has-text-weight-normal is-spaced">
-                    Aquí tienes algunas actividades en las que podemos emplear
-                    nuestro tiempo:
+                  <p className="list-title has-text-info has-text-weight-semibold is-spaced">
+                    Aquí tienes algunas actividades en las que podemos emplear nuestro tiempo:
                   </p>
                   <ul className="is-spaced">
                     <li>Pair programming</li>
@@ -36,20 +32,16 @@ function Pricing() {
                   </span>
                   <h1 className="title is-size-4 is-spaced">Impulsa tu carrera</h1>
                   <p className="is-spaced">
-                    Si ya tienes trabajo pero quieres saber qué te falta para
-                    ayudar mejor a tu equipo o conseguir un ascenso, este es el
-                    plan que estás buscando.
+                    Si ya tienes trabajo pero quieres saber qué te falta para ayudar mejor a tu equipo o conseguir un
+                    ascenso, este es el plan que estás buscando.
                   </p>
-                  <p className="list-title has-text-info has-text-weight-normal is-spaced">
+                  <p className="list-title has-text-info has-text-weight-semibold is-spaced">
                     Algunas ideas para inspirarte:
                   </p>
                   <ul className="is-spaced">
                     <li>Preparar un ascenso a senior</li>
                     <li>Técnicas de liderazgo</li>
-                    <li>
-                      Cómo presentar propuestas de mejora a tus compañeras y
-                      compañeros
-                    </li>
+                    <li>Cómo presentar propuestas de mejora a tus compañeras y compañeros</li>
                   </ul>
                 </div>
               </div>
@@ -60,18 +52,12 @@ function Pricing() {
                   </span>
                   <h1 className="title is-size-4 is-spaced">Consigue el trabajo</h1>
                   <p className="is-spaced">
-                    ¿Estás en uno o varios procesos de selección y necesitas
-                    ayuda para prepararlos? Déjame acompañarte durante el
-                    proceso.
+                    ¿Estás en uno o varios procesos de selección y necesitas ayuda para prepararlos? Déjame acompañarte
+                    durante el proceso.
                   </p>
-                  <p className="list-title has-text-info has-text-weight-normal is-spaced">
-                    Cómo puedo ayudarte:
-                  </p>
+                  <p className="list-title has-text-info has-text-weight-semibold is-spaced">Cómo puedo ayudarte:</p>
                   <ul className="is-spaced">
-                    <li>
-                      Practicar las preguntas típicas en entrevistas de
-                      Front-End
-                    </li>
+                    <li>Practicar las preguntas típicas en entrevistas de Front-End</li>
                     <li>Feedback sobre pruebas técnicas</li>
                     <li>Optimización del CV y portfolio</li>
                   </ul>
@@ -85,22 +71,17 @@ function Pricing() {
       <section className="section has-text-centered is-spaced">
         <div className="container is-narrow">
           <h1 className="title is-spaced">
-            Contrata 10 sesiones* por{" "}
-            <span className="has-text-primary">300€</span>
+            Contrata 10 sesiones* por <span className="has-text-primary">300€</span>
           </h1>
           <h3 className="subtitle is-size-5-desktop">
             O si lo prefieres, escríbeme para hacer una{" "}
             <u>
               <strong>primera consultoría gratuita</strong>
             </u>{" "}
-            para hacerme preguntas, discutir detalles y decidir si soy la
-            persona que estás buscando.
+            para hacerme preguntas, discutir detalles y decidir si soy la persona que estás buscando.
           </h3>
           <p className="is-spaced">
-            <em>
-              *Sesiones de 1 hora, a distribuir según nuestra mútua
-              disponibilidad.
-            </em>
+            <em>*Sesiones de 1 hora, a distribuir según nuestra mútua disponibilidad.</em>
           </p>
           <div className="buttons is-centered">
             <a
