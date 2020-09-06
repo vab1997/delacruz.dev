51d9c6cead2f6cc9c1c0d9dc134694c1339bd3d8
Dani de la Cruz <5173869+delacruz-dev@users.noreply.github.com>
2020-09-06 09:17:16 +0200

    add rss feed

---
+++ scripts/generate-rss.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const RSS = require("rss");
+const getPostsSync = require("./get-posts-sync");
+
+function dateSortDesc(a, b) {
+  const date1 = new Date(a.data.frontmatter.date);
+  const date2 = new Date(b.data.frontmatter.date);
+  if (date1 > date2) return -1;
+  if (date1 < date2) return 1;
+  return 0;
+}
+
+function generate() {
+  const previewItems = getPostsSync(false, null, "content/blog");
+  const feed = new RSS({
+    title: "Dani de la Cruz | Mentoring, tutoría y coaching a medida para programadores y programadoras que buscan dar un salto en su carrera profesional",
+    site_url: "https://delacruz.dev",
+    feed_url: "https://delacruz.dev/feed.xml",
+  });
+
+  previewItems.sort(dateSortDesc).map((post) => {
+    feed.item({
+      title: post.data.frontmatter.title,
+      guid: post.fileName,
+      url: `https://delacruz.dev/blog/${post.fileName}`,
+      date: post.data.frontmatter.date,
+      description: post.data.frontmatter.description,
+      author: post.data.frontmatter.author,
+    });
+  });
+
+  const rss = feed.xml({ indent: true });
+  fs.writeFileSync("./.next/static/feed.xml", rss);
+}
+
+generate();
---
+++ scripts/get-posts-sync.js
@@ -0,0 +1,34 @@
+const matter = require("gray-matter");
+const fs = require("fs");
+
+const getPostsSync = () => {
+  const contentDir = "posts";
+  const files = getLocalFiles(contentDir);
+  const posts = files.map((file) => {
+    const content = fs.readFileSync(`${file}`, "utf8");
+    const data = matter(content);
+    return {
+      fileName: file.substring(contentDir.length + 1, file.length - 3),
+      fileRelativePath: file,
+      data: {
+        frontmatter: {
+          description: data.data.description || "",
+          title: data.data.title,
+          date: data.data.date || "",
+          author: data.data.author || "",
+        },
+        markdownBody: data.content,
+      },
+    };
+  });
+  return posts;
+};
+
+const getLocalFiles = (filePath) => {
+  // grab all md files
+  const fg = require("fast-glob");
+  const files = fg.sync(`${filePath}**/*.md`);
+  return files;
+};
+
+module.exports = getPostsSync;
