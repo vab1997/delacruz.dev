7c1261e4722fe5f5f249c544a017486b74367ef9
Dani de la Cruz <5173869+delacruz-dev@users.noreply.github.com>
2020-09-06 09:43:13 +0200

    add rss (#13)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,32 @@
 module.exports = {
-  webpack: function (config) {
+  target: "serverless",
+  experimental: {
+    modern: true,
+    rewrites() {
+      return [
+        {
+          source: "/feed.xml",
+          destination: "/_next/static/feed.xml",
+        },
+      ];
+    },
+  },
+  webpack: function (config, { isServer, dev }) {
     config.module.rules.push({
       test: /\.md$/,
       use: "raw-loader",
     });
+
+    if (isServer && !dev) {
+      const originalEntry = config.entry;
+      config.entry = async () => {
+        const entries = { ...(await originalEntry()) };
+        // This script imports components from the Next app, so it's transpiled to `.next/server/scripts/build-rss.js`
+        entries['./scripts/generate-rss.js'] = './scripts/generate-rss.js';
+        return entries;
+      };
+    }
+
     return config;
   },
 };
