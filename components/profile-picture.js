df6f03eed8866e0ee0db0cda72ba1126db3b5de5
Dani de la Cruz <5173869+delacruz-dev@users.noreply.github.com>
2020-11-14 16:08:27 +0100

    extract profile picture as a component

---
+++ components/profile-picture.js
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+function ProfilePicture() {
+  return <>
+    <svg height="0" width="0">
+      <defs>
+        <clipPath id="blob" clipPathUnits="objectBoundingBox">
+          <path d="M0.75815095,0.0579477769 C0.879893708,0.187288937 0.902165272,0.677587654 0.799370955,0.785996249 C0.627963035,0.966765889 0.26163708,0.91434951 0.111342491,0.755791573 C-0.0332137967,0.603287436 -0.035795248,0.382887577 0.0965066612,0.173955315 C0.200239457,0.0101396315 0.648923894,-0.0580965318 0.75815095,0.0579477769 Z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+    <Image
+      src="/images/profile/me.jpg"
+      className='profile-picture'
+      alt="Dani de la Cruz"
+      width='500'
+      height='500'
+      loading='eager'
+    />
+  </>
+}
+export default ProfilePicture;
