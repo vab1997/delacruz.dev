d5ad68a01bca84857bf8b1edf1a549fb42d39a60
Dani de la Cruz <dani.delacruz@glovoapp.com>
2021-02-06 21:03:39 +0100

    Update error in experience

--- components/experience.js
+++ components/experience.js
@@ -26,23 +26,23 @@ function Experience() {
                   Desarrollo de aplicaciones web con{" "}
                   <a title="React" rel="nofollow noopener" href="https://reactjs.org/" target="_blank">
                     React
-                    </a>
+                  </a>
                 </li>
                 <li>
                   Desarrollo de aplicaciones móviles con{" "}
                   <a title="React Native" rel="nofollow noopener" href="https://reactnative.dev/" target="_blank">
                     React Native
-                    </a>
+                  </a>
                 </li>
                 <li>
                   Desarrollo de servicios y microservicios con{" "}
                   <a title="NestJS" rel="nofollow noopener" href="https://nestjs.com/" target="_blank">
                     NestJS
-                    </a>{" "}
-                    y{" "}
+                  </a>{" "}
+                  y{" "}
                   <a title="PostgreSQL" rel="nofollow noopener" href="https://www.postgresql.org/" target="_blank">
                     PostgreSQL
-                    </a>
+                  </a>
                 </li>
                 <li>
                   Despliegue de infraestructura en{" "}
@@ -53,11 +53,11 @@ function Experience() {
                     target="_blank"
                   >
                     Google Cloud Platform
-                    </a>{" "}
-                    con{" "}
+                  </a>{" "}
+                  con{" "}
                   <a title="Terraform" rel="nofollow noopener" href="https://www.terraform.io/" target="_blank">
                     Terraform
-                    </a>
+                  </a>
                 </li>
               </ul>
             </Box>
@@ -76,26 +76,26 @@ function Experience() {
               <ul>
                 <li>
                   <strong>Mentoring</strong> a compañeros y compañeras del departamento que deseaban mejorar en su
-                    especialidad o crecer asumiendo otras competencias
-                  </li>
+                  especialidad o crecer asumiendo otras competencias
+                </li>
                 <li>
                   Desarrollo de aplicaciones web con{" "}
                   <a title="React" rel="nofollow noopener" href="https://reactjs.org/" target="_blank">
                     React
-                    </a>
+                  </a>
                 </li>
                 <li>Participación en la definición de arquitectura de Front-End</li>
                 <li>
                   Pruebas unitarias con{" "}
                   <a title="Jest" rel="nofollow noopener" href="https://jestjs.io/" target="_blank">
                     Jest
-                    </a>
+                  </a>
                 </li>
                 <li>
                   Implementación de interfaz de usuario con{" "}
                   <a title="Ant Design" rel="nofollow noopener" href="https://ant.design/" target="_blank">
                     Ant Design
-                    </a>
+                  </a>
                 </li>
               </ul>
             </Box>
@@ -105,23 +105,17 @@ function Experience() {
             <Box
               media={
                 <a title="Workshare" target="_blank" rel="nofollow noopener" href="https://www.workshare.com/">
-                  <img
-                    loading="lazy"
-                    width="263"
-                    height="67"
-                    alt="Workshare Logo"
-                    src="/images/workshare-logo.png"
-                  />
+                  <img loading="lazy" width="263" height="67" alt="Workshare Logo" src="/images/workshare-logo.png" />
                 </a>
               }
             >
               <p className="heading">De marzo 2018 a julio 2019</p>
-              <h3>Senior Software Engineer at King</h3>
+              <h3>Senior Software Engineer at Workshare</h3>
               <ul>
                 <li>
                   <strong>Mentoría</strong> en la definición de proceso de trabajo ágil y entrega contínua sostenible
-                    respaldado por pruebas automáticas
-                  </li>
+                  respaldado por pruebas automáticas
+                </li>
                 <li>Contribución en la definición de arquitectura en el Front-End</li>
                 <li>Desarrollo de aplicaciones web con React</li>
               </ul>
@@ -141,37 +135,37 @@ function Experience() {
               <ul>
                 <li>
                   <strong>Mentoring</strong> a compañeros de equipo en desarrollo de software guiado por tests (
-                    <a
+                  <a
                     href="https://www.jamesshore.com/Agile-Book/test_driven_development.html"
                     title="Test-Driven development"
                     target="_blank"
                     rel="nofollow noopener"
                   >
                     TDD
-                    </a>
-                    ).
-                  </li>
+                  </a>
+                  ).
+                </li>
                 <li>Participación en la definición de arquitectura de Front-End</li>
                 <li>Liderazgo tecnológico de equipos de desarrollo</li>
                 <li>
                   Facilitación y construcción de una comunidad de Front-End basada en el aprendizaje y en compartir el
                   conocimiento
-                  </li>
+                </li>
                 <li>
                   Desarrollo de aplicaciones web con React y{" "}
                   <a title="Redux" target="_blank" rel="nofollow noopener" href="https://redux.js.org/">
                     Redux
-                    </a>
+                  </a>
                 </li>
                 <li>
                   Automatización de pruebas unitarias, integración y aceptación con{" "}
                   <a href="https://mochajs.org/" title="Mocha" target="_blank" rel="nofollow noopener">
                     Mocha
-                    </a>{" "}
-                    y{" "}
+                  </a>{" "}
+                  y{" "}
                   <a href="https://cucumber.io/" title="Cucumber" target="_blank" rel="nofollow noopener">
                     Cucumber
-                    </a>
+                  </a>
                 </li>
               </ul>
             </Box>
@@ -190,9 +184,8 @@ function Experience() {
               <ul>
                 <li>Liderazgo tecnológico de uno de los equipos de producto del portal Inmobiliario Fotocasa.</li>
                 <li>
-                  Liderazgo y ejecución de la transformación del Front-End de la compañía, de una plataforma
-                  monolítica en .NET hacia una Single-Page Application desacoplada del Back-End implementada con React
-                    y basada en{" "}
+                  Liderazgo y ejecución de la transformación del Front-End de la compañía, de una plataforma monolítica
+                  en .NET hacia una Single-Page Application desacoplada del Back-End implementada con React y basada en{" "}
                   <a
                     href="https://blog.cleancoder.com/uncle-bob/2012/08/13/the-clean-architecture.html"
                     title="Arquitectura Hexagonal"
@@ -200,9 +193,9 @@ function Experience() {
                     rel="nofollow noopener"
                   >
                     Arquitectura Hexagonal
-                    </a>
-                    .
-                  </li>
+                  </a>
+                  .
+                </li>
               </ul>
             </Box>
           </article>
@@ -228,14 +221,14 @@ function Experience() {
                   Desarrollo de{" "}
                   <a href="https://www.inmofactory.com/" title="Inmofactory" target="_blank" rel="nofollow noopener">
                     Inmofactory
-                    </a>
-                    , el CRM inmobiliario del grupo en España
-                  </li>
+                  </a>
+                  , el CRM inmobiliario del grupo en España
+                </li>
                 <li>
                   Diseño e implementación de búsquedas Near Real Time (NRT) con{" "}
                   <a href="https://lucene.apache.org/core/" title="Lucene" target="_blank" rel="nofollow noopener">
                     Lucene
-                    </a>
+                  </a>
                 </li>
                 <li>
                   Desarrollo con{" "}
@@ -246,7 +239,7 @@ function Experience() {
                     rel="nofollow noopener"
                   >
                     ASP.NET
-                    </a>
+                  </a>
                 </li>
               </ul>
             </Box>
@@ -255,12 +248,7 @@ function Experience() {
           <article className="timeline-item">
             <Box
               media={
-                <a
-                  href="https://dotnet.microsoft.com/"
-                  title=".NET Framework"
-                  target="_blank"
-                  rel="nofollow noopener"
-                >
+                <a href="https://dotnet.microsoft.com/" title=".NET Framework" target="_blank" rel="nofollow noopener">
                   <Image loading="lazy" width="263" height="67" alt="dotNET Logo" src="/images/dotNET-logo.png" />
                 </a>
               }
@@ -271,14 +259,14 @@ function Experience() {
                 Trabajé en consultorías como{" "}
                 <a href="https://www.everis.com/spain/es/home-1" rel="noopener nofollow" title="everis">
                   everis
-                  </a>{" "}
-                  o{" "}
+                </a>{" "}
+                o{" "}
                 <a title="ATOS" href="https://atos.net/es/spain" rel="noopener nofollow">
                   ATOS
-                  </a>
-                  , aprendiendo de grandes profesionales los fundamentos de programación que luego me servirían durante
-                  el resto de mi carrera profesional.
-                </p>
+                </a>
+                , aprendiendo de grandes profesionales los fundamentos de programación que luego me servirían durante el
+                resto de mi carrera profesional.
+              </p>
             </Box>
           </article>
         </div>
